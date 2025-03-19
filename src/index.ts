be60af9ea0d211a4f993d23958987be94bb9997d
aaron <aaron@telegiganten.dk>
2025-03-19 10:29:19 +0100

    feat: set trust proxy

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ import { initializeStockUpdateScheduler } from '@/controllers/schedulerControlle
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1); 
 
 // Configure CORS
 const corsOptions = {
